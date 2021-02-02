695699159535535981e333e15a21729b29d7e1db
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-02-02 20:43:36 +0100

    Update

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ```sh
 npm install
-```  
+```
 
 ### Load initial data
 
@@ -45,53 +45,52 @@ npm install
 
 ### **List of Ads**
 
-GET/api  
+GET/api
 
 {
 "tags": [
 "lifestyle",
 "motor"
 ],
-"_id": "5f5e012030f9c417ef8b40b2",
+"\_id": "5f5e012030f9c417ef8b40b2",
 "name": "Bicicleta",
 "state": "sell",
 "price": 230.15,
 "img": "bici.jpg",
-"__v": 0
+"\_\_v": 0
 },
 
-* Filters  
-  * img : <http://localhost:3000/api?img=bici.jpg>
-  * name : <http://localhost:3000/api?name=Iphone5>
-  * price: <http://localhost:3000/api?price=125.85>
-  * state : <http://localhost:3000/api?state=buy>
-  * tags : <http://localhost:3000/api?tags=lifestyle&tags=mobile>
-  * sort : <http://localhost:3000/api?sort=price>
-  * Max price: <http://localhost:3000/api?mxPrice=20>
-  * Min price: <http://localhost:3000/api?mnPrice=100>
-  * In range price: <http://localhost:3000/api?mnPrice=100&mxPrice=200>
-  * sort and skip : <http://localhost:3000/api?limit=1&skip=2>  
-  
-* Error response : error: {600} data no found
+- Filters
+  - img : <http://localhost:3000/api?img=bici.jpg>
+  - name : <http://localhost:3000/api?name=Iphone5>
+  - price: <http://localhost:3000/api?price=125.85>
+  - state : <http://localhost:3000/api?state=buy>
+  - tags : <http://localhost:3000/api?tags=lifestyle&tags=mobile>
+  - sort : <http://localhost:3000/api?sort=price>
+  - Max price: <http://localhost:3000/api?mxPrice=20>
+  - Min price: <http://localhost:3000/api?mnPrice=100>
+  - In range price: <http://localhost:3000/api?mnPrice=100&mxPrice=200>
+  - sort and skip : <http://localhost:3000/api?limit=1&skip=2>
+- Error response : error: {600} data no found
 
 ### **Create ad**
 
-POST/api/new {body: name: *required*, price: *required*, state: (buy || sell), tags: optionals, img: required }  
+POST/api/new {body: name: _required_, price: _required_, state: (buy || sell), tags: optionals, img: required }
 
-* *response*: {'Ad create', newAd{}}  
+- _response_: {'Ad create', newAd{}}
 
-* *error response* : error{
-    400: error.message
-}
+- _error response_ : error{
+  400: error.message
+  }
 
-### ***Delete Ad***
+### **_Delete Ad_**
 
-DELETE/api/del?_id  
-Returns: number of ads removed  
+DELETE/api/del?\_id  
+Returns: number of ads removed
 
 ### How to start a local mongodb instance for development
 
-``` ssh
+```ssh
 ./bin/mongod --dbpath ./data/db --directoryperdb
 ```
 
@@ -116,9 +115,8 @@ Change name file .env copy to .env
 
 Enter the required data
 
-
 ## Author
 
 👤 **Luis Sanchez Prudencio**
 
-* Github: [@LSP-92](https://github.com/LSP-92)
+- Github: [@LSP-92](https://github.com/LSP-92)
--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,82 @@
-var express = require('express')
-const path = require('path')
-var router = express.Router()
-const Ad = require('../models/Ad')
+var express = require("express");
+const path = require("path");
+var router = express.Router();
+const Ad = require("../models/Ad");
 
-router.get('/', async function (req, res, next) {
+router.get("/", async function (req, res, next) {
   try {
-    const sort = req.query.sort
-    const limit = parseInt(req.query.limit || 10)
-    const skip = parseInt(req.query.skip)
-    const name = req.query.name
-    const price = parseFloat(req.query.price)
-    const tags = req.query.tags
-    const state = req.query.state
-    const mnPrice = parseFloat(req.query.mnPrice || 0)
-    const mxPrice = parseFloat(req.query.mxPrice || 9999999.999)
+    const sort = req.query.sort;
+    const limit = parseInt(req.query.limit || 10);
+    const skip = parseInt(req.query.skip);
+    const name = req.query.name;
+    const price = parseFloat(req.query.price);
+    const tags = req.query.tags;
+    const state = req.query.state;
+    const mnPrice = parseFloat(req.query.mnPrice || 0);
+    const mxPrice = parseFloat(req.query.mxPrice || 9999999.999);
 
-    const filters = {}
-    if (name) { filters.name = new RegExp('^' + req.query.name) }
-    if (price) { filters.price = price }
-    if (tags) { filters.tags = tags }
-    if (state) { filters.state = state }
-
-    const valueRes = await Ad.filters(filters, limit, sort, skip, mnPrice, mxPrice)
-
-    if (valueRes.length === 0) {
-
-      const error = new Error('No hay resultados de busqueda') 
-      error.status = 404
-
-      return next(error) 
+    const filters = {};
+    if (name) {
+      filters.name = new RegExp("^" + req.query.name);
+    }
+    if (price) {
+      filters.price = price;
+    }
+    if (tags) {
+      filters.tags = tags;
+    }
+    if (state) {
+      filters.state = state;
     }
 
-    res.render('index', { title: 'NodePOP', value: valueRes })
+    const valueRes = await Ad.filters(
+      filters,
+      limit,
+      sort,
+      skip,
+      mnPrice,
+      mxPrice
+    );
+
+    if (valueRes.length === 0) {
+      const error = new Error("No hay resultados de busqueda");
+      error.status = 404;
+
+      return next(error);
+    }
+
+    res.render("index", { title: "NodePOP", value: valueRes });
   } catch (err) {
-    console.log(err)
-    next(err)
+    console.log(err);
+    next(err);
   }
-})
+});
 
-router.get('/img/:id', async function (req, res, next) {
-
+router.get("/img/:id", async function (req, res, next) {
   try {
-
-    const id = req.params.id
-    const valueRes = await Ad.findOne({ img: id })
+    const id = req.params.id;
+    const valueRes = await Ad.findOne({ img: id });
 
     if (!valueRes) {
+      const error = new Error("No image, check id");
+      error.status = 404;
 
-      const error = new Error('No image, check id')
-      error.status = 404
+      return next(error);
+    }
 
-      return next(error)
-    } 
-    
     //TODO no se puede devolver la ruta revisar __dirname ''probar''
-    res.sendFile(path.join('/home/luis/Nodepop/Nodepop/public/api/', valueRes.img))
-
+    res.sendFile(
+      path.join(
+        __dirname,
+        "/home/luis/Nodepop/Nodepop/public/api/",
+        valueRes.img
+      )
+    );
   } catch (err) {
-
-    const error = new Error(err)
-    error.status = 500
-    next(error)
+    const error = new Error(err);
+    error.status = 500;
+    next(error);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
